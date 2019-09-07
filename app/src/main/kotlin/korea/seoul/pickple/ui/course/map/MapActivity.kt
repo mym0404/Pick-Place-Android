5dfa382da81a1de1651678f8b2c680e85026c332
MJ Studio <mym0404@gmail.com>
2019-09-07 23:34:12 +0900

    MapActivity Update

--- app/src/main/kotlin/korea/seoul/pickple/ui/course/map/MapActivity.kt
+++ app/src/main/kotlin/korea/seoul/pickple/ui/course/map/MapActivity.kt
@@ -3,11 +3,18 @@ package korea.seoul.pickple.ui.course.map
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 import korea.seoul.pickple.R
 import korea.seoul.pickple.common.extensions.setShowSideItemsWithDimens
 import korea.seoul.pickple.databinding.ActivityMapBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+/**
+ * Activity for Course Google Map API representation
+ *
+ * @author MJ
+ */
 class MapActivity : AppCompatActivity() {
 
     /**
@@ -24,18 +31,42 @@ class MapActivity : AppCompatActivity() {
      */
     private val mViewModel : MapViewModel by viewModel()
 
+    /**
+     * [GoogleMap] Instance
+     */
+    private lateinit var mMap : GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMapBinding.inflate(LayoutInflater.from(this))
         setContentView(mBinding.root)
 
-
+        initMap()
         initMapPager()
     }
 
     /**
-     * ViewPager2 를 초기화하는 메서드
+     * [GoogleMap] Initialization from [SupportMapFragment] in [MapActivity]
+     *
+     * @see GoogleMap
+     * @see SupportMapFragment
+     * @see mMap
+     */
+    private fun initMap() {
+        val mapFrag = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFrag.getMapAsync {
+            mMap = it
+            adjustMapLocation(mMap)
+        }
+    }
+
+    private fun adjustMapLocation(map : GoogleMap) {
+
+    }
+
+    /**
+     * Initialization ViewPager2
      */
     private fun initMapPager() {
         mBinding.viewPager2.apply {
