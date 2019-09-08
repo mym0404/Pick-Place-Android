684b298bc7b7a2a20f524c1617f6faabbb9734ac
MJ Studio <mym0404@gmail.com>
2019-09-09 03:23:21 +0900

    DirectionsAPI

--- app/src/main/kotlin/korea/seoul/pickple/data/api/DirectionsAPI.kt
+++ app/src/main/kotlin/korea/seoul/pickple/data/api/DirectionsAPI.kt
@@ -1,5 +1,18 @@
 package korea.seoul.pickple.data.api
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface DirectionsAPI {
+    @GET("/")
+    fun getRouteFromTwoPlace(
+        @Query("origin") origin : String,
+        @Query("destination") destination : String,
+        @Query("key") key : String,
+        @Query("mode") mode : String = "driving",
+
+
+        ) : Call<Unit>
 
 }
