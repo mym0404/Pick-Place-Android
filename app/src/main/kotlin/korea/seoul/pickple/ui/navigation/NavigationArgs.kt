c1020584c50592bf68c8a368b8c4a7d4a2670f74
MJ Studio <mym0404@gmail.com>
2019-09-13 21:53:34 +0900

    Navigator Animation Update

--- app/src/main/kotlin/korea/seoul/pickple/ui/navigation/NavigationArgs.kt
+++ app/src/main/kotlin/korea/seoul/pickple/ui/navigation/NavigationArgs.kt
@@ -2,6 +2,10 @@ package korea.seoul.pickple.ui.navigation
 
 import android.app.Activity
 import android.content.Intent
+import android.view.View
+import androidx.annotation.AnimRes
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import korea.seoul.pickple.data.entity.Course
 import korea.seoul.pickple.ui.course.map.MapActivity
 
@@ -29,14 +33,30 @@ fun MapActivity.parseIntent(intent : Intent) =   NavigationArgs.MapActivityArg(i
 /**
  * 네비게이션 할 때 쓰셈
  */
-fun Activity.navigateWithArgs(arg: NavigationArgs) = navigate(this,arg)
-fun navigate(curActivity: Activity, arg: NavigationArgs) {
+fun navigate(curActivity: Activity, arg: NavigationArgs,
+             @AnimRes enterResId : Int? = null,
+             @AnimRes exitResId : Int? = null,
+             sharedElementPairs : List<Pair<View,String>>? = null
+) {
 
+    //계속 추가 요망
     val intent = when (arg) {
         is NavigationArgs.MapActivityArg -> Intent(curActivity, MapActivity::class.java).apply {
             putExtra(NavigationArgs.MapActivityArg.MAP_ARG_COURSE, arg.course)
         }
     }
 
-    curActivity.startActivity(intent)
+
+
+    if(enterResId != null && exitResId != null) {
+
+        curActivity.startActivity(intent,ActivityOptionsCompat.makeCustomAnimation(curActivity,enterResId,exitResId).toBundle())
+        return
+    }else if(sharedElementPairs != null) {
+        curActivity.startActivity(intent,ActivityOptionsCompat.makeSceneTransitionAnimation(curActivity,*sharedElementPairs.toTypedArray()).toBundle())
+        return
+    }else {
+        curActivity.startActivity(intent)
+        return
+    }
 }
