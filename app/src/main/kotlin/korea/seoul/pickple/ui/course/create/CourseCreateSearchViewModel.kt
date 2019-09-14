7b487cd04da4b56b47a040d6333c9357c7bec0c7
MJ Studio <mym0404@gmail.com>
2019-09-14 18:01:30 +0900

    CourseCreateSearchActivity ui update

--- app/src/main/kotlin/korea/seoul/pickple/ui/course/create/CourseCreateSearchViewModel.kt
+++ app/src/main/kotlin/korea/seoul/pickple/ui/course/create/CourseCreateSearchViewModel.kt
@@ -1,7 +1,19 @@
 package korea.seoul.pickple.ui.course.create
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import korea.seoul.pickple.common.widget.Once
 
 class CourseCreateSearchViewModel : ViewModel() {
 
+    private val _clickBack : MutableLiveData<Once<Boolean>> = MutableLiveData<Once<Boolean>>()
+    val clickBack : LiveData<Once<Boolean>>
+        get() = _clickBack
+
+
+
+    fun onClickBackButton() {
+        _clickBack.value = Once(true)
+    }
 }
