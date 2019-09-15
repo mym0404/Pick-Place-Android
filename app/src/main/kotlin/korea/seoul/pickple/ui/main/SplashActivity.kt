f6bf11f40c12b31d2fa9da8ed01a8f8eea033aca
chloe73 <esom735@gmail.com>
2019-09-15 23:12:06 +0900

    Merge branch 'develop' of https://github.com/mym0404/Pick-Place-Android
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

--- app/src/main/kotlin/korea/seoul/pickple/ui/main/SplashActivity.kt
+++ app/src/main/kotlin/korea/seoul/pickple/ui/main/SplashActivity.kt
@@ -1,12 +1,14 @@
-package korea.seoul.pickple
+package korea.seoul.pickple.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import korea.seoul.pickple.R
 
-class LoginActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_splash)
+
     }
 }
