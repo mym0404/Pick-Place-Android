72550bfd1f137fa22f4e89d1e4ca58d0e69c4892
MJ Studio <mym0404@gmail.com>
2019-09-15 23:09:11 +0900

    Place Search Add

--- app/src/main/kotlin/korea/seoul/pickple/ui/course/create/search/CourseCreateSearchViewModel.kt
+++ app/src/main/kotlin/korea/seoul/pickple/ui/course/create/search/CourseCreateSearchViewModel.kt
@@ -16,13 +16,13 @@ class CourseCreateSearchViewModel : ViewModel() {
     //region Data
     private val _places : MutableLiveData<List<Place>> = MutableLiveData(listOf(
         //TODO DUMMY
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
-        Place(-1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa")
+        Place(1,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
+        Place(2,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
+        Place(3,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
+        Place(4,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
+        Place(5,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
+        Place(6,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa"),
+        Place(7,Place.Type.FOOD,"경복궁","경복경복",null,null,null,999,"aaa")
     ))
     val places : LiveData<List<Place>>
         get() = _places
@@ -33,10 +33,18 @@ class CourseCreateSearchViewModel : ViewModel() {
     private val _clickBack : MutableLiveData<Once<Boolean>> = MutableLiveData<Once<Boolean>>()
     val clickBack : LiveData<Once<Boolean>>
         get() = _clickBack
+
+    private val _clickAdd : MutableLiveData<Once<Place>> = MutableLiveData()
+    val clickAdd : LiveData<Once<Place>>
+        get() = _clickAdd
     //endregion
 
 
     fun onClickBackButton() {
         _clickBack.value = Once(true)
+    }
+
+    fun onClickAddButton(place : Place) {
+        _clickAdd.value = Once(place)
     }
 }
