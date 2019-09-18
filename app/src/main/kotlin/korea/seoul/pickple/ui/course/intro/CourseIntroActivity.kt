a1ab60b58942e77e977eb625bc52685655aee0cb
greedy0110 <greedy0110@gmail.com>
2019-09-19 06:46:25 +0900

    course intro에 공용 app bar 설치 완료
    공유하기 기능 구현 아직 안함 (공유하기에 대한 자세한 로직이 나와야함)
    전체 코스 보기 화면으로 넘어가기 아직 안함 (전체 코스 보기 화면을 만들어야함)

--- app/src/main/kotlin/korea/seoul/pickple/ui/course/intro/CourseIntroActivity.kt
+++ app/src/main/kotlin/korea/seoul/pickple/ui/course/intro/CourseIntroActivity.kt
@@ -1,6 +1,12 @@
 package korea.seoul.pickple.ui.course.intro
 
+import android.animation.ObjectAnimator
+import android.animation.StateListAnimator
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.appcompat.app.ActionBar
+import androidx.viewpager.widget.ViewPager
 import korea.seoul.pickple.R
 import korea.seoul.pickple.databinding.ActivityCourseIntroBinding
 import korea.seoul.pickple.ui.BaseActivity
@@ -9,14 +15,75 @@ import org.koin.core.parameter.parametersOf
 
 class CourseIntroActivity : BaseActivity<ActivityCourseIntroBinding>(R.layout.activity_course_intro) {
     private val viewModel: CourseIntroViewModel by viewModel { parametersOf(0) }
+    private var toolbarActionShare: MenuItem? = null
+    private var courseId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mBinding.vpCourseIntro.adapter = CourseIntroViewPagerAdapter(
-            supportFragmentManager
-        )
+        mBinding.apply {
+            vpCourseIntro.adapter = CourseIntroViewPagerAdapter(
+                supportFragmentManager
+            )
+            vpCourseIntro.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener(){
+                override fun onPageSelected(position: Int) {
+                    when(position) {
+                        0 -> {
+                            // appbar 끔
+                            supportActionBar?.hide()
+                        }
+                        1 -> {
+                            // course에 관련된 appbar
+                            supportActionBar?.show()
+                            toolbarActionShare?.isVisible = true
+                        }
+                        2 -> {
+                            // place 관련 appbar 그러면서도, place 아이디마다 달라질 수 있음
+                            supportActionBar?.show()
+                            toolbarActionShare?.isVisible = false
+                        }
+                    }
+                }
+            })
 
-        viewModel.courseId = 0
+            setSupportActionBar(toolbarCourseIntro)
+            supportActionBar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+                // 첫 화면에서 abb bar는 가린 상태!
+                hide()
+            }
+            // shadow를 없에기 위해서 추가
+            appbarLayoutCourseIntro.outlineProvider = null
+        }
+
+        viewModel.courseId = courseId
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_unite_course_toolbar, menu)
+        toolbarActionShare = menu?.findItem(R.id.actionShare)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            android.R.id.home -> {
+                // 액티비티를 종료하자.
+                Log.d("seungmin", "네비게이션 버튼")
+                finish()
+                true
+            }
+            R.id.actionShare -> {
+                // TODO 코스 공유하기 버튼임!
+                Log.d("seungmin", "$courseId 공유버튼")
+                true
+            }
+            R.id.actionShowAll -> {
+                // TODO 전체 코스 보기 화면으로 넘어가야함
+                Log.d("seungmin", "전체 코스보기")
+                true
+            }
+            else -> onOptionsItemSelected(item)
+        }
     }
 }
