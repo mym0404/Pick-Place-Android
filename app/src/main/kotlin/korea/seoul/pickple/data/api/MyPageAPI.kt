54a1aabc7f1a3022531c1d4af6b261d1bcb25740
ChoSooMin <vywlgns1102@naver.com>
2019-09-27 19:22:57 +0900

    before pull

--- app/src/main/kotlin/korea/seoul/pickple/data/api/MyPageAPI.kt
+++ app/src/main/kotlin/korea/seoul/pickple/data/api/MyPageAPI.kt
@@ -3,6 +3,7 @@ package korea.seoul.pickple.data.api
 import korea.seoul.pickple.data.api.response.mypage.*
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface MyPageAPI {
     @GET("mypage/seoul")
